8fab517d5b1c8bc7a33673741c3b6796f3b3ac87
S K <xz64@users.noreply.github.com>
2017-02-09 16:13:17 +0000

    style: add eslint fixes

--- logger.js
+++ logger.js
@@ -3,15 +3,15 @@ const winston = require('winston');
 const logger = new winston.Logger({
   transports: [
     new winston.transports.Console({
-      timestamp: function() {
-        var date = new Date();
+      timestamp() {
+        const date = new Date();
         return `${date.getTime()} ${date.toISOString()}`;
       },
-      formatter: function(options) {
+      formatter(options) {
         return `${options.timestamp()} ${options.level} ${options.message}`;
-      }
-    })
-  ]
+      },
+    }),
+  ],
 });
 
 module.exports = logger;
