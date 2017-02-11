ddd1ac99ca489c375c4bc8cfd95bec94e87de276
S K <xz64@users.noreply.github.com>
2017-02-11 17:57:03 +0000

    feat: add password hashing

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
+const co = require('co');
 const argon2 = require('argon2');
+const logger = require('../logger');
 
 const Schema = mongoose.Schema;
 
@@ -10,6 +12,44 @@ const userSchema = new Schema({
   city: String,
   state: String,
 });
+
+const hashOptions = {
+  timeCost: 100,
+  type: argon2.argon2i,
+};
+
+const hashPassword = co.wrap(function* passwordHasher(password) {
+  const salt = yield argon2.generateSalt();
+  const hash = yield argon2.hash(password, salt, hashOptions);
+  return hash;
+});
+
+userSchema.pre('save', function passwordMiddleware(next) {
+  if (this.isModified('password')) {
+    hashPassword(this.password)
+    .then((hash) => {
+      this.password = hash;
+      next();
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      logger.error(error);
+      next(error);
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.verifyPassword = function verifyPassword(givenPassword) {
+  const userPassword = this.password;
+  return co(function* passwordHashVerifier() {
+    const match = yield argon2.verify(userPassword, givenPassword);
+    if (!match) {
+      throw new Error('Password did not match');
+    }
+  });
+};
 
 const User = mongoose.model('User', userSchema);
 
