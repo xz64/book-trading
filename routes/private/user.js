e764fed96970ccfbfb27a0f3d2422eaf95939523
S K <xz64@users.noreply.github.com>
2017-06-25 01:29:53 +0000

    feat: add user id to user info api

--- routes/private/user.js
+++ routes/private/user.js
@@ -13,8 +13,9 @@ const validateProfile = validator({
 });
 
 router.get('/user', function* userInfo() { // eslint-disable-line require-yield
-  const { username, fullname, city, state } = this.passport.user;
+  const { _id, username, fullname, city, state } = this.passport.user;
   this.body = {
+    id: _id,
     username,
     fullname,
     city,
